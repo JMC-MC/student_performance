f636c8155b74b226aba9b74ff629f2fa5eddebbb
JMC-MC <joecurrin@192-168-1-104.tpgi.com.au>
2021-10-24 17:54:56 +1100

    Code for loading packages and data. Added to report intro.

--- code.R
+++ code.R
@@ -1,8 +1,22 @@
+##################################################
 
-d1 <- read.csv("data/student-mat.csv",sep=";",header=TRUE)
-d2 <- read.csv("data/student-por.csv",sep=";",header=TRUE)
+# Main code for Student Performance Project EDX 
 
-d3 <- merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
-print(nrow(d3)) # 382 students
+##################################################
 
-head(d2)
+# Check for and install required packages
+
+list_of_packages <- c("ggplot2", "Rcpp")
+new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
+
+#Attach required packages
+lapply(list_of_packages,library,character.only = TRUE)
+
+# Load data from local file using relative file path
+
+df_math <- read.csv("data/student-mat.csv",sep=";",header=TRUE)
+df_port <- read.csv("data/student-por.csv",sep=";",header=TRUE)
+
+
+
