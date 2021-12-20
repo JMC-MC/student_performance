41eda6123790bf42d1eb5aaf9f40fa3c7ea0115d
JMC-MC <joecurrin@Joes-MacBook-Pro.local>
2021-12-21 10:49:41 +1100

    Lots of new content fo the report and some adjustments to the code based on mistakes identified during report writing.

--- code.R
+++ code.R
@@ -27,50 +27,32 @@ df_port <- read.csv("data/student-por.csv",sep=";",header=TRUE)
  
 #Check for NAs and blanks
 na_math <- df_math[rowSums(is.na(df_math)) > 0,]
+na_math
 # No NAs or blanks in the maths class data. 
 na_port <- df_port[rowSums(is.na(df_port)) > 0,]
 # No NAs or blanks in the port class data.
 
 # Divide data into training and validation set
 set.seed(2017)
-train_index = createDataPartition(y=df_port$G3, p=0.9, list=FALSE)
+train_index <- createDataPartition(y=df_port$G3, p=0.9, list=FALSE)
 
-train_set = df_port[train_index,]
-valid_set = df_port[-train_index,]
+train_set <- df_port[train_index,]
+valid_set <- df_port[-train_index,]
 
 ##################################################
 # Explore
 ##################################################
 # Variable types
 str(train_set)
+
 # Majority of variable are categorical
-
-# Convert all variables to numerical 
-train_set
-
-describe(train_set)
+des<-describe(train_set)
 # The following categorical variables have more than 80% of students in a single category. Pstatus, Failures, Schoolsup, Paid, nursery, higher
-
-
-# Look at using factors
-
-describe(train_set)
-
-
-# Basic representation of data proportions
-# Plot showing the first 3 features.
-plot_port <- train_set %>% select(school,sex,age) %>% 
-  group_by(age,sex,school) %>% summarise(count=n())
-
-ggplot(plot_port) +
-  geom_col(aes(x = age, y = ifelse(plot_port$sex== "M",count,-count), fill = sex)) +
-  coord_flip() + 
-  facet_wrap(~ school) +  
-  scale_y_continuous(labels = function(n) scales::comma(abs(n))) +
-  ylab("Count") 
+des$Mjob
+des$Fjob
+# The majority of response for Mjob and Fjob are "other". This provides minimal insight
 
 # G3 distribution
-
 ggplot(data = train_set,aes(G3)) + geom_histogram(binwidth = 1)
 
 # How many 0 G3 scores are there in the train_set
@@ -84,34 +66,12 @@ zero_G3_G2_G1
 # This suggests that it is likely that a score of 0 was given to students who did not complete the test
 G3_min <- train_set %>% filter(G3>0 & G3<5)
 G3_min
-# Only 1 student scored less the 1 
+# Only 1 student scored less than 5. 
 
-# Does age have a relationship with G3
-train_set %>% ggplot(aes(age,G3))+geom_point() + geom_smooth()
-# There isn't enough date in the 20+ region to make and confident conclusions
-
-# Does studytime have a relationship with G3
-train_set %>% ggplot(aes(studytime,G3))+geom_point() + geom_smooth()
-
-# Does gender have a relationship with G3
-train_set %>% ggplot(aes(sex,G3))+geom_boxplot()
-# Slightly lower mean G3 for men
-# I have already confirmed the data is reasonably balanced in terms of sex
-
-# Is there a difference in G3 between schools?
-train_set %>% ggplot(aes(school,G3))+geom_boxplot()
-# MS appears to have lower scores with the mean and quartiles below GP
-
-#Is that a relationship between address and G3?
-train_set %>% ggplot(aes(address,G3))+geom_boxplot()
-# A slight change however mostly overlapped
-
-#Is that a relationship between famsize and G3?
-train_set %>% ggplot(aes(famsize,G3))+geom_boxplot()
-# No significant change in G3 between GT3 & LE3
 
 ##################################################
 # Preprocessing
+##################################################
 
 # Remove outliers, G3=<1 
 # It is assumed that these students did not complete the test.
@@ -121,7 +81,6 @@ ts_p0 <- train_set %>% filter(G3>1)
 ts_p1 <- ts_p0 %>% select(-c(Pstatus, failures, schoolsup, paid, nursery, higher))
 
 # Remove Mjob and Fjob, most responses in the other category and therefore provide very little information. 
-
 ts_p2 <- ts_p1 %>% select(-c(Mjob,Fjob))
 
 #Convert all chr variables to factors
@@ -166,7 +125,7 @@ cor(ts_p4$school,ts_p4$address)
 # We know that school is correlated with G3 and will therefore keep this predictor and remove the others
 ts_p8 <- ts_p7%>% select(-c(address,traveltime))
 
-#We are not going to use the variable G2 & G1 as we are trying to make predIctions early in the education process. 
+#We are not going to use the variable G2 & G1 as we are trying to make predictions early in the education process. 
 ts_p9 <- ts_p8%>% select(-c(G1,G2))
 
 # Normalize excluding G3 the data for multiple regression with the exception of the value being predicted. 
@@ -181,7 +140,7 @@ stan_pp <- predict(processed,as.data.frame(ts_p9))
 
 
 ##################################################
-# Develop model
+# Develop models
 ##################################################
 # Calculate RMSE
 RMSE <- function(true_score, predicted_score){
@@ -198,18 +157,25 @@ RMSE <- function(true_score, predicted_score){
 # Multivariate Linear Regression
   
   # Fit the model using all predictor
+  set.seed(2009)
   mul_reg_fit <- train(G3~., data = mul_reg_pp,method="lm",trControl = trainControl(method = "repeatedcv",number = 10,repeats = 3))
   mul_reg_fit$results
   summary(mul_reg_fit)
   # Remove insignificant predictors 
+  set.seed(2009)
   mul_reg_fit <- train(G3~school+sex+studytime+reason+guardian+famsup+Pedu+Aalc+health+absences , data = mul_reg_pp,method="lm",trControl = trainControl(method = "repeatedcv",number = 10,repeats = 3))
   mul_reg_fit$results
+  #Set graphical parameter to plot all final model graphs together.
+  par(mfrow=c(4,1))
+  # Plot residual graphs
   plot(mul_reg_fit$finalModel)
+  #Reset parameters ready for next plot
+  par(mfrow=c(1,1))
   
 # Observation from the residuals vs fit plot
     # The residuals are partially randomly distributed around 0.
     # The relationship appears close to linear.
-    # However the data is not equally spread across the score range so ther is some uncertainty.
+    # However the data is not equally spread across the score range so there is some uncertainty.
     # 3 outliers identified 309,373,562
 # Observation from the residuals vs fit plot
   d<-density(mul_reg_fit$finalModel$residuals)
@@ -219,9 +185,10 @@ RMSE <- function(true_score, predicted_score){
   # No observations are outside cook's distance.
   
   # Eliminate extreme values
-  mul_reg_pp <- mul_reg_pp[-which(rownames(mul_reg_pp)%in% c("309","373","309")),]
+  mul_reg_pp <- mul_reg_pp[-which(rownames(mul_reg_pp)%in% c("562","373","309")),]
 
   # Correct with skew with log
+  set.seed(2009)
   mul_reg_fit_log <- train(log10(G3)~school+sex+studytime+reason+guardian+famsup+Pedu+Aalc+health+absences, data = mul_reg_pp,method="lm",trControl = trainControl(method = "repeatedcv",number = 10,repeats = 3))
   # Plot with log
   d_l<-density(mul_reg_fit_log$finalModel$residuals)
@@ -229,7 +196,7 @@ RMSE <- function(true_score, predicted_score){
   # Inverse log to get RMSE
   skew_adu_results <- RMSE(mul_reg_pp$G3,10^mul_reg_fit_log$finalModel$fitted.values)
   skew_adu_results
-  #RMSE improved from 2.39 to 2.33
+  #RMSE improved from 2.39 to 2.31
   
 # Penalized Linear Regression 
   # Use standardized data to avoid the model becoming affected by the scale of the predictors. 
@@ -288,7 +255,6 @@ RMSE <- function(true_score, predicted_score){
   plot(rf_fit_tunes)
   # optimal mtry = 4
   min(rf_fit_tunes$results$RMSE)
-  
   # Take a look at variable importance
   varImp(rf_fit)
   
@@ -304,6 +270,7 @@ RMSE <- function(true_score, predicted_score){
   plot(knn_fit)
   # k = 9 gives the lowest RMSE however intergers above 9 were not tested. 
   # Tune using grid method
+  set.seed(2017)
   k_list <- seq(1,60,2)
   knn_fit_tuned = train(G3 ~ ., 
                   data=ts_p9, 
@@ -311,16 +278,15 @@ RMSE <- function(true_score, predicted_score){
                   metric = "RMSE",
                   preProcess = c("center", "scale"),
                   savePredictions= TRUE,
-                  trControl = trainControl(method="repeatedcv", number=10, repeats=3))
+                  trControl = trainControl(method="repeatedcv", number=10, repeats=3),
+                  tuneGrid = expand.grid(k = k_list))
   plot(knn_fit_tuned)
-  # optimal k=25
+  # optimal k=31
   min(knn_fit_tuned$results$RMSE)
-  knn_pred <- predict(knn_fit_tuned, newdata = train_set)
-  length(knn_pred,)
-  length(train_set$G3)
-  RMSE(train_set$G3,knn_pred)
+
   
   #SVM 
+  set.seed(2017)
   svm_fit = train(G3 ~ ., 
                   data=stan_pp, 
                   method="svmLinear",
@@ -329,6 +295,7 @@ RMSE <- function(true_score, predicted_score){
   svm_fit
   # RMSE 2.42 without tuning 
   # Tune for C parameter
+  set.seed(2017)
   c_list <- seq(0.001,0.02,0.001)
   svm_fit_tuned = train(G3 ~ ., 
                   data=stan_pp, 
@@ -337,34 +304,75 @@ RMSE <- function(true_score, predicted_score){
                   trControl = trainControl(method="repeatedcv", number=10, repeats=3)
                   )
   plot(svm_fit_tuned)
-  #The final value used for the model was C = 0.016 
+  svm_fit_tuned$bestTune
+  #The final value used for the model was C = 0.013
+  min(svm_fit_tuned$results$RMSE)
+  
   
 # Ensemble
   # Using caret list I am unable to tune each model individual so this will be left to be done automatically
   # The multivariate regression cannot be used due to the adjustments that have been made outside of the model
+  set.seed(2017)  
   model_list <- caretList(G3 ~ .,
-                          data=stan_pp,
-                          trControl = trainControl(method="repeatedcv", number=10, repeats=3, savePredictions = "final"),
-                          methodList = c("glmnet", "rf", "knn","svmLinear"),
-                          tuneList = NULL,
-                          continue_on_fail = FALSE)
-set.seed(17)
+    data=stan_pp,
+    trControl = trainControl(method="repeatedcv", number=10, repeats=3, savePredictions = "final"),
+    methodList = c("lm","glmnet", "rf", "knn","svmLinear"),
+    tuneList = NULL,
+    continue_on_fail = FALSE)
+  set.seed(2017)
   # Create stack using rf
   rf_ensemble <- caretStack(
     model_list,
     method="rf",
     metric="RMSE",
-    trControl=trainControl(
-      method="boot",
-      number=10,
-      savePredictions="final"
-    )
+    trControl=trainControl(method="repeatedcv", number=10, repeats=3, savePredictions = "final")
   )
- rf_ensemble$ens_model
- #RMSE 2.32
+  rf_ensemble$ens_model
+  varImp(rf_ensemble$ens_model)
+  min(rf_ensemble$error$RMSE)
+ #RMSE 2.35
+ 
+##################################################
+# Compare models using RMSE
+##################################################
  
 # Create comparison Data Frame
 model_comp <- tibble(Model=c("Naive","Multivariate Regression","Penalized Linear Regression","Regression Tree","Random Forest","KNN","SVM","Ensemble"),
        RMSE=c(naive_RMSE,skew_adu_results,min(glmnet_fit_tune$results$RMSE),min(r_tree$results$RMSE),min(rf_fit_tunes$results$RMSE),min(knn_fit_tuned$results$RMSE),min(svm_fit_tuned$results$RMSE),min(rf_ensemble$error$RMSE)))
 model_comp %>% arrange(by=RMSE)
-  
+
+# The top performing models are Ensemble & Multivariate Regression.
+
+##################################################
+# Validate selected models
+##################################################
+# Adjust validation set to include Pedu & Aalc
+valid_set_adj <- valid_set %>% mutate(Pedu = (Medu+Fedu)/2,Aalc = (Dalc+Walc)/2) %>% select(-c(Medu,Fedu,Dalc,Walc))
+#Convert all chr variables to factors & then to numeric
+valid_set_adj_num <- valid_set_adj %>% mutate_if(is.character,as.factor) %>% mutate_if(is.factor,as.numeric)
+
+# Validate Ensemble model
+ens_pred <- predict(rf_ensemble,valid_set_adj_num)
+final_ens_RMSE <-  RMSE(valid_set_adj_num$G3,ens_pred)
+final_ens_RMSE
+
+# Create results data frame
+results <- data.frame(predictions=ens_pred,scores=valid_set_adj_num$G3,errors=valid_set_adj_num$G3-ens_pred)
+ggplot(data = results,aes(errors)) + geom_histogram(binwidth = 1)
+
+# Summarize errors
+summary(results$errors)
+sd(results$errors)
+
+ggplot(data = results,aes(predictions)) + geom_histogram(binwidth = 1)
+ggplot(data = results,aes(scores)) + geom_histogram(binwidth = 1)
+
+
+
+# Validate Multi Variate Regression model
+mul_reg_pred <- predict(mul_reg_fit_log,valid_set_adj_num)
+final_multi_reg_RSME <- RMSE(valid_set_adj_num$G3,10^mul_reg_pred)
+
+# The high RMSE for the Multi Variate Regression model compared to the RMSE during training suggests that the model has been over fitted
+
+
